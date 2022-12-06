ae7980e483335a2e0127c7815c9b3f59472f71dc
Tarık <trkefe08@gmail.com>
2022-12-06 14:20:56 +0300

    The list screen is divided into folders and models are written for GameList

---
+++ FinalProject/FinalProject/Scene/GameList/Model/GameListModel.swift
@@ -0,0 +1,31 @@
+//
+//  GameListModel.swift
+//  FinalProject
+//
+//  Created by Tarik Efe on 6.12.2022.
+//
+
+import Foundation
+
+struct GameListModel: Codable {
+    let count: Int?
+    let next: String?
+    //let previous: JSONNull?
+    let results: [GameListResultModel]?
+    let seoTitle, seoDescription, seoKeywords, seoH1: String?
+    let noindex, nofollow: Bool?
+    let gameListModelDescription: String?
+    //let filters: Filters?
+    let nofollowCollections: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case count, next, results
+        case seoTitle = "seo_title"
+        case seoDescription = "seo_description"
+        case seoKeywords = "seo_keywords"
+        case seoH1 = "seo_h1"
+        case noindex, nofollow
+        case gameListModelDescription = "description"
+        case nofollowCollections = "nofollow_collections"
+    }
+}
