758fe8291313a8de36cc5cf92d7f202f977ea4c1
Tarık <trkefe08@gmail.com>
2022-12-08 12:35:24 +0300

    GameListViewModel is written using protocol and delegate

--- FinalProject/FinalProject/Scene/GameList/ViewModel/GameListViewModel.swift
+++ FinalProject/FinalProject/Scene/GameList/ViewModel/GameListViewModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+//MARK: Protocol
+protocol GameListViewModelProtocol {
+    var delegate: GameListViewModelDelegate? { get set }
+    func fetchGamesList()
+    func getGamesCount() -> Int
+    func getGamesId(at index: Int) -> Int?
+}
+//MARK: Delegate
+protocol GameListViewModelDelegate: AnyObject {
+    func gamesLoaded()
+}
+
+final class GameListViewModel: GameListViewModelProtocol {
+    weak var delegate: GameListViewModelDelegate?
+    private var games: [GameListResultModel]?
+    
+    //MARK: Methods
+    func fetchGamesList() {
+        RawgDBClient.shared.getGamesList { [weak self] result in
+            switch result {
+            case .success(let results):
+                DispatchQueue.main.async {
+                    self?.games = results
+                    self?.delegate?.gamesLoaded()
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func getGamesCount() -> Int {
+        games?.count ?? 0
+    }
+    
+    func getMovie(at index: Int) -> GameListResultModel? {
+        games?[index]
+    }
+    
+    func getGamesId(at index: Int) -> Int? {
+        games?[index].id
+    }
+    
+}
