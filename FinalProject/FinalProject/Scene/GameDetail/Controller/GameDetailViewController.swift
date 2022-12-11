bd26c2f28734ee44602fdc9b2ae0b09913722932
Tarık <trkefe08@gmail.com>
2022-12-11 19:45:44 +0300

    Created ViewController and ViewModel for GameDetail

---
+++ FinalProject/FinalProject/Scene/GameDetail/Controller/GameDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameDetailViewController.swift
+//  FinalProject
+//
+//  Created by Tarik Efe on 11.12.2022.
+//
+
+import UIKit
+
+class GameDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ FinalProject/FinalProject/Scene/GameDetail/ViewModel/GameDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GameDetailViewModel.swift
+//  FinalProject
+//
+//  Created by Tarik Efe on 11.12.2022.
+//
+
+import Foundation
