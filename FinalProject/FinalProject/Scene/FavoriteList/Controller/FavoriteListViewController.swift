55f7f8cc5c07c6ab61f2fff77d5a5f71e389371a
Tarık <trkefe08@gmail.com>
2022-12-13 10:58:06 +0300

    Necessary folders have been created for FavoriteList and tableview outlet has been assigned

---
+++ FinalProject/FinalProject/Scene/FavoriteList/Controller/FavoriteListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteListViewController.swift
+//  FinalProject
+//
+//  Created by Tarik Efe on 13.12.2022.
+//
+
+import UIKit
+
+class FavoriteListViewController: UIViewController {
+
+    @IBOutlet private weak var favoriteTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ FinalProject/FinalProject/Scene/FavoriteList/ViewModel/FavoriteListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteListViewModel.swift
+//  FinalProject
+//
+//  Created by Tarik Efe on 13.12.2022.
+//
+
+import Foundation
