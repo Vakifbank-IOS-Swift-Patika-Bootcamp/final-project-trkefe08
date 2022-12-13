ff26d7fd82ea3205de4c2a7fd47e6809697360f5
Tarık <trkefe08@gmail.com>
2022-12-13 13:15:39 +0300

    Necessary protocol, delegate, function operations are written for FavoriteListViewModel

--- FinalProject/FinalProject/Scene/FavoriteList/ViewModel/FavoriteListViewModel.swift
+++ FinalProject/FinalProject/Scene/FavoriteList/ViewModel/FavoriteListViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+//MARK: Protocols
+protocol FavoriteListViewModelProtocol {
+    var delegate: FavoriteListViewModelDelegate? { get set }
+    func fetchGamesFromCoreData()
+    func getFavoriteGameCount() -> Int
+    func getFavorite(at index: Int) -> FinalProject?
+    func getFavoriteId(at index: Int) -> Int64?
+}
+
+protocol FavoriteListViewModelDelegate: AnyObject {
+    func favoritesLoaded()
+}
+
+//MARK: Class
+final class FavoriteListViewModel: FavoriteListViewModelProtocol {
+    weak var delegate: FavoriteListViewModelDelegate?
+    var favoriteGames: [FinalProject] = []
+    
+    //MARK: Methods
+    func fetchGamesFromCoreData() {
+        CoreDataManager.shared.getFavoriteGamesFromCoreData { result in
+            switch result {
+            case .success(let favorites):
+                self.favoriteGames = favorites
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func getFavoriteGameCount() -> Int {
+        favoriteGames.count
+    }
+    
+    func getFavorite(at index: Int) -> FinalProject? {
+        favoriteGames[index]
+    }
+    
+    func getFavoriteId(at index: Int) -> Int64? {
+        favoriteGames[index].id
+    }
+    
+}
