7770c9d296af092d981728e247f3e8c84c7e8fd2
Tarık <trkefe08@gmail.com>
2022-12-15 12:59:24 +0300

    Folders were made for the Note screen and the storyboard was updated

---
+++ FinalProject/FinalProject/Scene/NoteList/Controller/NoteListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NoteListViewController.swift
+//  FinalProject
+//
+//  Created by Tarik Efe on 15.12.2022.
+//
+
+import UIKit
+
+class NoteListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ FinalProject/FinalProject/Scene/NoteList/ViewModel/NoteListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoteListViewModel.swift
+//  FinalProject
+//
+//  Created by Tarik Efe on 15.12.2022.
+//
+
+import Foundation
